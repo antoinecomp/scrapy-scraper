8989074d69a7f38581ecec330d57c0cef84ea2a1
AntoineCompagnie <antoine.compagnie@gmail.com>
2020-12-03 13:04:10 +0100

    commented import

--- nosetime_scraper/spiders/__init__.py
+++ nosetime_scraper/spiders/__init__.py
@@ -2,4 +2,4 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
-from .nosetime_spider import NosetimeScraper
+# from .nosetime_spider import NosetimeScraper
